317cf101cf31bb340e860a1b703296cdce14214e
Tameka Roberson <TTRoberson@student.fullsail.edu>
2024-05-28 19:05:34 -0400

    Task 2 WIP

--- Task-2.py
+++ Task-2.py
@@ -1,1 +1,26 @@
-# Advanced Data Handling w/ Python
+# # Advanced Data Handling w/ Python
+
+# hotel_rooms = {
+#     "101": {"status": "available", "customer": ""},
+#     "102": {"status": "booked", "customer": "John Doe"}
+# }
+
+# hotel_rooms["103"] = {"status": "booked", "customer": "Bobby Brown"}
+# hotel_rooms["102"]["status"] = "available"
+# hotel_rooms["102"]["customer"] = ""
+
+
+# for i, x in hotel_rooms.items():
+#     for y in x:
+#         if "booked" in x[y]:
+            
+        
+# print(hotel_rooms)
+            
+            
+           
+
+# for i, x in hotel_rooms.items():
+#     for y in x:
+#         if y == "status":
+#             print(x[y])
