fc99c457137aca69f1470140de58b0bfc7036e06
Tameka Roberson <TTRoberson@student.fullsail.edu>
2024-05-29 13:53:14 -0400

    Task 2 Completed

--- Task-2.py
+++ Task-2.py
@@ -1,26 +1,23 @@
-# # Advanced Data Handling w/ Python
+# Advanced Data Handling w/ Python
 
-# hotel_rooms = {
-#     "101": {"status": "available", "customer": ""},
-#     "102": {"status": "booked", "customer": "John Doe"}
-# }
+hotel_rooms = {
+    "101": {"status": "available", "customer": ""},
+    "102": {"status": "booked", "customer": "John Doe"}
+}
 
-# hotel_rooms["103"] = {"status": "booked", "customer": "Bobby Brown"}
-# hotel_rooms["102"]["status"] = "available"
-# hotel_rooms["102"]["customer"] = ""
+hotel_rooms["103"] = {"status": "booked", "customer": "Bobby Brown"}
 
-
-# for i, x in hotel_rooms.items():
-#     for y in x:
-#         if "booked" in x[y]:
-            
+for i, x in hotel_rooms.items():
+    for y in x:
+        if y == "status":
+           x[y] = "available"
+        if y == "customer":
+               x[y] = ""
         
-# print(hotel_rooms)
-            
-            
-           
+print(hotel_rooms)
 
-# for i, x in hotel_rooms.items():
-#     for y in x:
-#         if y == "status":
-#             print(x[y])
+            
+for i, x in hotel_rooms.items():
+    for y in x:
+        if y == "status":
+            print(f"Room {i}'s {y}: {x[y]}")
