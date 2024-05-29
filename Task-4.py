b4d25a1fee9e61f5cfe7ea18bcc6aa65664b34b5
Tameka Roberson <TTRoberson@student.fullsail.edu>
2024-05-29 14:02:36 -0400

    Task 4 completed

--- Task-4.py
+++ Task-4.py
@@ -1,1 +1,17 @@
 # Python Essentials for Business Analytics
+
+weekly_sales = {
+    "Week 1": {"Electronics": 12000, "Clothing": 5000, "Groceries": 7000},
+    "Week 2": {"Electronics": 15000, "Clothing": 6000, "Groceries": 8000}
+}
+
+weekly_sales_2 = weekly_sales.copy()
+
+for i, x in weekly_sales_2.items():
+    if i == "Week 2":
+        for y in x:
+            if y == "Electronics":
+                x[y] = 18000
+
+print(weekly_sales)
+print(weekly_sales_2)
